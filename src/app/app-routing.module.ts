3e1f0d0b1fe948c16140f00aef45282f45d31aed
Carol <carolbenetao@gmail.com>
2021-02-10 17:58:33 -0300

    avaliação Carol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"header",
+    component: HeaderComponent
+  },
+  {
+    path:"footer",
+    component: FooterComponent
+  },
+  {
+    path:"menu",
+    component: MenuComponent
+  },
+  {
+    path:"",
+    component: DashboardComponent
+  },
+  {
+    path:"blocks",
+    component: BlocksComponent
+  },
+  {
+    path:"cards",
+    component: CardsComponent
+  },
+  {
+    path:"forms",
+    component: FormsComponent
+  },
+  {
+    path:"pricing",
+    component: PricingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    DashboardComponent,
+    BlocksComponent,
+    CardsComponent,
+    FormsComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
